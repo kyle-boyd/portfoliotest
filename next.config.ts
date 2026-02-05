20af535e7c3f6872200fee118f9388c12edb99b2
Kyle Boyd <105263259+kyle-boyd@users.noreply.github.com>
2026-02-05 15:27:26 -0700

    fix issue

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/portfolio/" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/portfoliotest" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/portfoliotest/" : "",
 };
 
 export default nextConfig;
